54124fee55349d626944b306c66136d3b65f5e49
russlarge256 <45131808+russlarge256@users.noreply.github.com>
2019-07-27 17:44:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 import sys
 import json
-
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
-
 if __name__ == "__main__":
-    username = sys.argv[1]
+    username = 'russlarge256'
+    response = requests.get(f"https://api.github.com/users/{username}/events")
+    events = json.loads(response.content)
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
+    print(events[0]['created_at'])
     
 
 
